ea7a0aeee8c3cac9e2fe87cb0fdfee33ea33d272
CoolBro4811 <piano4life010@gmail.com>
2025-03-27 14:44:36 -0700

    adding folder

--- counselor/models.py
+++ counselor/models.py
@@ -2,4 +2,5 @@ from django.db import models
 from django.forms import CharField
 from django.utils import timezone
 
-# Create your models here.
+class User(models.Model):
+    # var = models.charfield()...
