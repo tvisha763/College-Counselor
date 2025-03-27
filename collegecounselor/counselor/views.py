4f540ce559f0f8c31a199d69b8111190dc335bb9
Tvisha Choubey <76886688+tvisha763@users.noreply.github.com>
2025-03-27 17:08:01 +0000

    home page

--- collegecounselor/counselor/views.py
+++ collegecounselor/counselor/views.py
@@ -4,7 +4,6 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .models import User, Post, Recycle_Event
 import bcrypt
 import requests
 import urllib
@@ -15,3 +14,7 @@ from django.conf import settings
 from django.template.loader import render_to_string
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
