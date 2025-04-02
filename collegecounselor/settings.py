956f37f75df96bebabffedf411dbd6bdb2ba9671
Tvisha Choubey <76886688+tvisha763@users.noreply.github.com>
2025-04-02 16:41:50 +0000

    fixed file upload

--- collegecounselor/settings.py
+++ collegecounselor/settings.py
@@ -124,5 +124,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'resumes')
-MEDIA_URL = '/resumes/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
