30e9d0686777ea42304a27db30bd26680caf0c79
Colin Lambert <piano4life010@gmail.com>
2025-06-22 20:59:30 -0700

    fixing password hasher

--- counselor/hashers.py
+++ counselor/hashers.py
@@ -2,7 +2,5 @@ from django.contrib.auth.hashers import Argon2PasswordHasher
 
 
 class CustomArgon2PasswordHasher(Argon2PasswordHasher):
-    def __init__():
-        super().__init__()
-        self.time_cost = super().time_cost * 10
-        self.memory_cost = super().memory_cost * 3
+    time_cost = super().time_cost * 10
+    memory_cost = super().memory_cost * 3
